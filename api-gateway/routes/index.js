ed386ee15c00872768b4c03698b7f7a34c5626be
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-05-27 23:44:43 -0400

    updating files

--- api-gateway/routes/index.js
+++ api-gateway/routes/index.js
@@ -1,8 +1,18 @@
+/*
+============================================
+; Title: Assignment 4.3
+; Author: Professor Krasso
+; Date: 24 May 2020
+; Modified By: Jonathan Kobyluck
+; Description: API Gateway Part III
+;===========================================
+*/
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
