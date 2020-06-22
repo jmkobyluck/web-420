c821d40db9a3bbce5df5206b89c915c6afddd70c
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-06-21 22:22:49 -0400

    Adding Assignment 8.4

--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -8,20 +8,15 @@
 ;===========================================
 */
 
-var express = require('express');
+var express = require("express");
+var checkToken = require("../check-token");
 var router = express.Router();
-var auth_controller = require('../controllers/authController');
+var auth_controller = require("../controllers/authController");
 
-// POST request for registering a user
-router.post('/auth/register', auth_controller.user_register);
-
-// GET request for verifying user tokens
-router.get('/auth/token', auth_controller.user_token);
-
-// allow user login requests
-router.post('/auth/login', auth_controller.user_login);
-
-// allow user logout requests
-router.get('/auth/logout', auth_controller.user_logout);
+router.get("/auth/token", checkToken, auth_controller.user_token);
+router.post("/auth/login", auth_controller.user_login);
+router.post("/auth/register", auth_controller.user_register);
+router.get("/auth/token", auth_controller.user_token);
+router.get("/auth/logout", auth_controller.user_logout);
 
 module.exports = router;
